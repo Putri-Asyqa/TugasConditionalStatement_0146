beb91bd2e0ac11bb48f43647f510009cfb2d3353
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-03-10 20:04:31 +0700

    menambahakan void

--- tugas.cpp
+++ tugas.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
+// Prosedur untuk memasukkan data
+void inputData(double &berat, double &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
